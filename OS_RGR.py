f0c053e523ad0bb23b2a52fc40228c9088039618
Mamoru9 <glebphibo@gmail.com>
2019-05-24 00:35:15 +0300

    prod

--- OS_RGR.py
+++ OS_RGR.py
@@ -11,21 +11,25 @@ for i in range(500):
     data.insert(i, 0)
 
 temp = 0
+flag = True
 
 while (True):
     print("Чтобы выйти нажмите ctrl+c") 
     writer = int(input("Введите кол-во квантов записи: "))
-    reader_start = int(input("Введите квант с которого начинает читать читатель: "))
+    if flag:
+        reader_start = int(input("Введите квант с которого начинает читать читатель: "))
+        flag = False
     reader = int(input("Введите кол-во квантов чтения:"))
     while writer!=0:
         sheet.cell(row=2, column=temp+2).fill=PatternFill(start_color='008000', end_color='008000', fill_type='solid')
+        if flag != True:
+            sheet.cell(row=3, column=temp+2).fill=PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
         writer-=1
         data[temp]=1
         temp+=1
-    #temp+=reader_start
     tmp_bool = True
     while reader!=0:
-        if tmp_bool:
+        if tmp_bool and flag:
             while True:
                 if data[reader_start-1]==1:
                     sheet.cell(row=3, column=reader_start+1).fill=PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
